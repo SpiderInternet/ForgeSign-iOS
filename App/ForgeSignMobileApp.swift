89186b6106dd62eae96f7a77862efc69483072f7
SpiderInternet <osn35906533@gmail.com>
2026-08-11 17:33:07 +0300

    Fix: Provide CertificateStore as EnvironmentObject at app root to prevent CertificatesSheet crash

--- App/ForgeSignMobileApp.swift
+++ App/ForgeSignMobileApp.swift
@@ -3,11 +3,13 @@ import SwiftUI
 @main
 struct ForgeSignMobileApp: App {
     @StateObject private var sourceStore = SourceStore()
+    @StateObject private var certStore = CertificateStore()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(sourceStore)
+                .environmentObject(certStore)
         }
     }
 }
